edbe2c619886fceeb60aeace598fa7b9ecd466dc
RuairiSpain <lapido@gmail.com>
2017-07-27 19:16:56 +0200

    Start ES6 sample output

---
+++ index.js
@@ -0,0 +1,12 @@
+function* simple(){
+    yield 10;
+    yield 20;
+    return 30;
+}
+
+const gen = simple();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+
