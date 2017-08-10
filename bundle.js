d82a93bb45768e534fd3eca906d9851c02af1d38
Ruairi O'Donnell <lapido@gmail.com>
2017-08-10 18:07:38 +0200

    fix

---
+++ bundle.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *
+ * @returns generator
+ */
+function* simple() {
+  yield 10;
+  yield 20;
+  return 30;
+}
+
+const gen = simple();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
